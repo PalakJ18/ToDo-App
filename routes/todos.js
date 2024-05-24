50b129d3779b5469e5780d5e8a51c833a6be90ae
Palak Jain <106911776+PalakJ18@users.noreply.github.com>
2024-05-24 16:54:26 +0530

    Add files via upload

---
+++ routes/todos.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+//import controller
+const { createTodo } = require("../controllers/createTodo");
+const { getTodos,getTodoById } = require("../controllers/getTodo.js");
+const { updateTodo} = require("../controllers/updateTodo");
+const { deleteTodo} = require("../controllers/deleteTodo");
+
+//define APi routes
+// path is mapped with controller
+router.post("/createTodo", createTodo);
+router.get("/getTodos", getTodos);
+router.get("/getTodos/:id", getTodoById);
+router.put("/updateTodo/:id", updateTodo);
+router.delete("/deleteTodo/:id", deleteTodo);
+
+module.exports = router;
